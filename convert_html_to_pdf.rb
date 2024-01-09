3e207a8d035d7256ffc6ca44a4126ac3fd83b93b
Richie Thomas <rickthomas1980@gmail.com>
2024-01-09 12:52:46 -0700

    Reference correct CSS file in PDF converter

--- convert_html_to_pdf.rb
+++ convert_html_to_pdf.rb
@@ -8,7 +8,7 @@ converter.configure do
   output 'output.pdf'
   resource_path 'assets/images'
   filter "filter_remove_slashes_from_images.rb"
-  css "pdf_styles2.css"
+  css "pdf_styles.css"
   pdf_engine 'wkhtmltopdf'
 end
 
